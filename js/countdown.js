354e29df5c26a789ad9cc762e04782935c306738
Sander Milloen <sander.milloen@gmail.com>
2021-12-01 23:16:53 +0100

    first commit

---
+++ js/countdown.js
@@ -0,0 +1,39 @@
+
+function countdown(){
+    var now = new Date(Date.now());
+    var eventDate = new Date("December 12, 2021 20:00:00");
+
+    var currentTiime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTiime;
+    
+    if(remTime <= 0) {
+        document.getElementById('countdownContainer').style.display = 'none';
+        return;
+    }
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("days").innerText = d;
+
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
---
+++ js/main.js
@@ -0,0 +1,108 @@
+var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+const eventTime = ((new Date("December 12, 2021 20:00:00")).getTime()) /1000;
+const CONTRACT_ADDRESS = "0x9CD4f3D3EE197EF245D1dA5dbeB4Bfe412886Db1";
+document.getElementById("price").textContent = 0.5
+
+
+async function checkLogin() {
+    let user = await web3.eth.getAccounts();
+    if (!user[0]) {
+        document.getElementById("btn-login").addEventListener("click", async () =>{
+            const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+            if (!accounts) {
+                alert('no acc detected, make one');
+                return;
+            } else {
+                renderThings();
+            }    
+        });
+    } else {
+        renderThings();
+    }  
+}
+
+checkLogin();
+
+async function renderThings(){
+    document.getElementById("btn-login").style.display = "none";
+    document.getElementById("MustBeLoggedIn").style.display = "none";
+    let abi = await getAbi();
+    let contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+    let price = await contract.methods.price().call({});
+    document.getElementById("price").textContent = price/1000000000000000000;
+    contract.methods.totalSupply().call({}).then( (result) => {
+        document.getElementById("totalSupply").textContent = result;
+        if (checkTime()==true){
+            if (result>= 10) {
+                document.getElementById("message").textContent = "Unfortunately, minting is over... Better luck next time!";
+                document.getElementById("cost").style.display = "none";
+    
+            } else if (result == 9){
+                document.getElementById("message").textContent = "There is only " + (10-result) + " masterpiece left, you better be fast!";
+                document.getElementById("mint-button").style.display = "block";
+    
+            } else {
+                document.getElementById("message").textContent = "There are only " + (10-result) + " masterpieces left, you better be fast!";
+                document.getElementById("mint-button").style.display = "block";    
+            }            
+        } else {
+            document.getElementById("message").textContent = "Unfortunately, minting hasn't started yet";
+            var i = setInterval(function() {
+                if (checkTime() === true){
+                    document.getElementById("message").textContent = "There are only " + (10-result) + " masterpieces left, you better be fast!";
+                    document.getElementById("mint-button").style.display = "block";
+                    clearInterval(i);
+                }
+              }, 1000);
+        }
+    })
+}
+
+document.getElementById("mint-button").addEventListener("click", async () => {
+    document.getElementById("mint-button").style.display = "none";
+    let abi = await getAbi();
+    let contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+    let account = await web3.eth.getAccounts();
+    let price = await contract.methods.price().call({});
+    console.log(account[0]);
+    contract.methods.mint(1).send({
+        from: account[0],
+        value: price,
+    }).once("error", (err) => {
+        console.log(err);
+        document.getElementById("mint-button").style.display = "block";
+    }).then((receipt) => {
+        console.log("succes")
+        contract.methods.totalSupply().call({}).then( (result) => {  
+            document.getElementById("totalSupply").textContent = result;  
+            if (result>= 10) {
+                document.getElementById("message").textContent = "Unfortunately, minting is over... Better luck next time!";
+                document.getElementById("mint-button").style.display = "none";
+                document.getElementById("cost").style.display = "none";
+            } else if (result == 9){
+                document.getElementById("message").textContent = "There is only " + (10-result) + " masterpiece left, you better be fast!";
+                document.getElementById("mint-button").style.display = "block"; 
+            } else {
+                document.getElementById("message").textContent = "There are only " + (10-result) + " masterpieces left, you better be fast!";
+                document.getElementById("mint-button").style.display = "block";    
+            }   
+        })
+    })
+})
+
+function getAbi(){
+    return new Promise( (res) => {
+        $.getJSON("/abi.json", ( (json) => {
+            res(json);
+        }))
+    })}
+
+function checkTime(){
+    var now = new Date(Date.now());
+    if ((now.getTime())/1000 >= eventTime){
+        return true;
+    } else {
+        return false;
+    }
+}
+ 
